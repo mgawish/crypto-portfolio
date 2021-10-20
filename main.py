a7b2c3ebb60c643c0a4165d1b82490561628a8f3
mgawish <mohd.gawish@gmail.com>
2021-10-20 17:02:54 +0400

    refactored script into class

--- main.py
+++ main.py
@@ -7,136 +7,153 @@ from decouple import config
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 import requests
+import pandas as pd
 
 #Read config
-filepath = os.path.dirname(os.path.realpath(__file__))
-config = open(filepath + '/config.json')
-data = json.load(config)
-offline_assets = data['offline_assets']
-target_allocations = data['target_allocations']
-total_invested = data['total_invested']
-DISCORD_WEBHOOK_URL = data['discord_webhook_url']
-API_KEY = data['binance_api_key']
-API_SECRET = data['binance_api_secret']
-GOOGLE_SHEET_ID = data['google_sheet_id']
-GOOGLE_SHEET_NAME = data['google_sheet_name']
+class Main():
+    filepath = os.path.dirname(os.path.realpath(__file__))
+    config = open(filepath + '/config.json')
+    data = json.load(config)
+    offline_assets = data['offline_assets']
+    target_allocations = data['target_allocations']
+    total_invested = data['total_invested']
+    DISCORD_WEBHOOK_URL = data['discord_webhook_url']
+    API_KEY = data['binance_api_key']
+    API_SECRET = data['binance_api_secret']
+    GOOGLE_SHEET_ID = data['google_sheet_id']
+    GOOGLE_SHEET_NAME = data['google_sheet_name']
 
-#Binance api
-client = Spot(key=API_KEY, secret=API_SECRET)
-account = client.account()
-balances = account['balances']
+    columns = ['asset',
+               'price',
+               'amount',
+               'value',
+               'alloc',
+               'target_alloc',
+               'alloc_diff',
+               'amount_diff']
+    total_value = 0
+    total_alloc_value = 0
+    notification_required = False
 
-#Generate portfolio rows
-assets = []
-total_usd_value = 0
-alloc_total = 0
-notification_required = False
-for b in balances:
-    asset = b['asset']
-    free = float(b['free'])
-    locked = float(b['locked'])
-    amount = free + locked
+    def run_strategy(self):
+        df = self.generate_assets()
+        json_data = self.generate_overview(df)
+        self.update_google_sheet(json_data)
+        if self.notification_required:
+            self.send_notification()
 
+    def generate_assets(self):
+        client = Spot(key=self.API_KEY, secret=self.API_SECRET)
+        account = client.account()
+        balances = account['balances']
+        assets = []
 
-    if offline_assets.get(asset) != None:
-        amount += offline_assets.get(asset)
+        for b in balances:
+            asset = b['asset']
+            free = float(b['free'])
+            locked = float(b['locked'])
+            amount = free + locked
 
-    if amount == 0:
-        continue
+            if self.offline_assets.get(asset) != None:
+                amount += self.offline_assets.get(asset)
 
-    if asset == 'USDT':
-        price = 1
-    else:
-        price = float(client.ticker_price(f'{asset}USDT')['price'])
+            if amount == 0:
+                continue
 
-    usdValue = amount * price
-    total_usd_value += usdValue
-    assets.append([asset, price, amount])
+            if asset == 'USDT':
+                price = 1
+            else:
+                price = float(client.ticker_price(f'{asset}USDT')['price'])
 
-    if target_allocations.get(asset) != None:
-        alloc_total += usdValue
+            if self.target_allocations.get(asset) != None:
+                target_alloc = self.target_allocations.get(asset)
+            else:
+                target_alloc = 0
 
-for i in range(len(assets)):
-    a = assets[i]
-    asset = a[0]
-    price = a[1]
-    amount = a[2]
-    usd_value = price * amount
-    alloc = usd_value / total_usd_value
+            value = amount * price
+            assets.append({
+                'asset': asset,
+                'price': price,
+                'amount': amount,
+                'target_alloc': target_alloc
+            })
 
-    if target_allocations.get(asset) != None:
-        target_alloc = target_allocations.get(asset)
-        alloc_diff = target_alloc - alloc
-        amount_change = (target_alloc * alloc_total - usd_value) / price
-    else:
-        target_allocation = 0
-        alloc_diff = 0
-        amount_change = 0
+        df = pd.DataFrame(data=assets, columns=self.columns)
+        df['value'] = df['price'] * df['amount']
+        self.total_value = df['value'].sum()
 
-    if abs(alloc_diff) > target_alloc * 0.1:
-        notification_required = True
+        alloc_portfolio = df.loc[df['target_alloc'] != 0]
+        self.total_alloc_value = alloc_portfolio['value'].sum()
 
-    assets[i] = [asset,
-                 price,
-                 amount,
-                 usd_value,
-                 alloc,
-                 target_alloc,
-                 alloc_diff,
-                 amount_change]
+        df['alloc'] = 0
+        df.loc[df['target_alloc'] != 0, 'alloc'] = df['value'] / self.total_alloc_value
+        df['alloc_diff'] = df['target_alloc'] - df['alloc']
 
+        df['amount_diff'] = 0
+        df.loc[df['target_alloc'] != 0, 'amount_diff'] = (df['target_alloc'] * self.total_alloc_value - df['value']) / df['price']
 
-#Add overview
-overview = []
-overview.append(['Investment', total_invested])
-overview.append(['Current value', total_usd_value])
-overview.append(['Percentage', total_usd_value / total_invested])
-now = datetime.now()
-overview.append(['Last updated at', now.strftime("%d/%m/%Y %H:%M:%S")])
-overview.append([])
-overview.append(['Asset',
-                 'Price',
-                 'Amount',
-                 'USD Value',
-                 'Alloc',
-                 'Target Alloc',
-                 'Alloc Diff',
-                 'Amount Diff'])
-overview = overview + assets
+        df = df.sort_values('value', ascending=False)
 
-#Send discord notification if rebalancing is required
-if notification_required:
-    payload = {
-        "username": "CryptoBot",
-        "content": "Portfolio requires rebalancing"
-    }
-    response = requests.post(DISCORD_WEBHOOK_URL, json=payload)
+        dff =  df[abs(df['alloc_diff']) > df['target_alloc'] * 0.1]
+        if not dff.empty:
+            self.notification_required = True
 
-#Write portfolio table to google sheet
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-credentials = None
-gs_keys = filepath + '/crypto-portfolio-key.json'
-credentials = service_account.Credentials.from_service_account_file(gs_keys,
-                                                                    scopes=SCOPES)
-service = build('sheets', 'v4', credentials=credentials)
-sheet = service.spreadsheets()
+        return df
 
-#Clear sheet
-range = f'{GOOGLE_SHEET_NAME}!A1:Z'
-body = {}
-response = service.spreadsheets().values().clear(spreadsheetId=GOOGLE_SHEET_ID,
-                                                 range=range,
-                                                 body=body).execute()
-#Update sheet
-range = f'{GOOGLE_SHEET_NAME}!A1'
-body = {
-    'values': overview
-}
-request = sheet.values().update(spreadsheetId=GOOGLE_SHEET_ID,
-                                range=range,
-                                valueInputOption='USER_ENTERED',
-                                body=body).execute()
-#Print goes to log
-print()
-print(now)
-print(request)
+    def generate_overview(self, df):
+        overview = []
+        overview.append(['Investment', self.total_invested])
+        overview.append(['Current value', self.total_value])
+        overview.append(['Percentage', self.total_value / self.total_invested])
+        now = datetime.now()
+        overview.append(['Last updated at', now.strftime("%d/%m/%Y %H:%M:%S")])
+        overview.append([])
+
+        cols = df.columns.tolist()
+        cols = [str.capitalize() for str in cols]
+        cols = [str.replace('_',' ') for str in cols]
+
+        overview.append(cols)
+        overview = overview + df.values.tolist()
+        return overview
+
+    def send_notification(self):
+        print('send_notification')
+        payload = {
+            "username": "CryptoBot",
+            "content": "Portfolio requires rebalancing"
+        }
+        response = requests.post(self.DISCORD_WEBHOOK_URL, json=payload)
+
+    def update_google_sheet(self, data):
+        SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        credentials = None
+        gs_keys = self.filepath + '/crypto-portfolio-key.json'
+        credentials = service_account.Credentials.from_service_account_file(gs_keys,
+                                                                            scopes=SCOPES)
+        service = build('sheets', 'v4', credentials=credentials)
+        sheet = service.spreadsheets()
+
+        #Clear sheet
+        range = f'{self.GOOGLE_SHEET_NAME}!A1:Z'
+        body = {}
+        response = service.spreadsheets().values().clear(spreadsheetId=self.GOOGLE_SHEET_ID,
+                                                         range=range,
+                                                         body=body).execute()
+        #Update sheet
+        range = f'{self.GOOGLE_SHEET_NAME}!A1'
+        body = {
+            'values': data
+        }
+        request = sheet.values().update(spreadsheetId=self.GOOGLE_SHEET_ID,
+                                        range=range,
+                                        valueInputOption='USER_ENTERED',
+                                        body=body).execute()
+        #Print goes to log
+        print()
+        print(datetime.now())
+        print(request)
+
+if __name__ == '__main__':
+    main = Main()
+    main.run_strategy()
